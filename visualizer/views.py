e0af5882dd1c6cad4f73d9e20bf40313ba3be278
JustAProgrammer01234 <JustAProgrammerlol@gmail.com>
2021-02-12 13:20:23 +0800

    I finally found out how django knows if the user clicks the button, did a testing if elif statements in views.py!

--- visualizer/views.py
+++ visualizer/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
+    if 'genarr' in request.GET:
+        print('Generating array')
+    elif 'bubble' in request.GET:
+        print('Sorting array using bubble sort')
+    elif 'insertion' in request.GET:
+        print('Sorting array using insertion sort')
+        
     return render(request, 'visualizer/templates.html')
